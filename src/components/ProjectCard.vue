76f5ccdf5e4e0acc55f3d029b96dde4276e3d6e7
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-09-12 10:59:03 +0200

    added better ProjectCard structure & display

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -8,11 +8,46 @@ export default {
 </script>
 
 <template>
-    <h1>Projects</h1>
+    <h1 class="mb-3">Projects</h1>
 
-    <ul>
-        <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
-    </ul>
+    <div v-for="project in projects" :key="project.id">
+        <div class="card px-5">
+            <img :src="project.image" id="project-image" class="card-img-top mt-2 shadow-lg" alt="Project Image">
+            <div class="card-body">
+                <h5 class="card-title">{{ project.title }}</h5>
+                <p class="card-text">{{ project.description }}</p>
+                <div class="row">
+                    <div class="col-4">
+                        <p><strong>Client:</strong> {{ project.client }}</p>
+                        <p><strong>Completion Year:</strong> {{ project.completion_year }}</p>
+                    </div>
+                    <div class="col-4">
+                        <p><strong>Project Duration:</strong> {{ project.project_duration }}</p>
+                        <p><strong>Category:</strong> {{ project.category.label }}</p>
+                    </div>
+                    <div class="col-4">
+                        <div class="technologies mb-3">
+                            <strong>Technologies:</strong>
+                            <span v-for="tech in project.technologies" :key="tech.id" class="badge badge-secondary">{{
+                                tech.label
+                            }}</span>
+                        </div>
+                        <a :href="project.url">Visit Website</a>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#project-image {
+    height: 450px;
+    width: 900px;
+    object-fit: cover;
+    margin: 0 auto;
+}
+</style>
