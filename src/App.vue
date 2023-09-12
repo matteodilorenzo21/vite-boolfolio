20c84eb85649ee1adab327dd5023a34f3a927d9f
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-09-12 08:52:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>Ciao Mondo</h1>
+  </div>
+</template>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+// import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.vue'
+
+const app = createApp(App);
+
+app.mount('#app')
