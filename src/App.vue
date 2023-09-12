e7f7db028eb05c5bbdc82111200684d9775b3320
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-09-12 09:09:44 +0200

    axios import into app.vue & endpoint declared

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script>
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects';
 import AppHeader from './components/AppHeader.vue';
 export default {
   components: { AppHeader }
