c7d8739252375f863def1c6db49e14edf5f5cfeb
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-09-12 09:13:46 +0200

    added data function for projects api into app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import axios from 'axios';
 const endpoint = 'http://localhost:8000/api/projects';
 import AppHeader from './components/AppHeader.vue';
 export default {
-  components: { AppHeader }
+  components: { AppHeader },
+  data: () => ({ projects: [] }),
 };
 </script>
 
