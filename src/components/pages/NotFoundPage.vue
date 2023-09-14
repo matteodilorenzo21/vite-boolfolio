ea305cd2d29c36d9ca5d545c2c148c8c7564193d
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-09-14 08:09:59 +0200

    added PageNotFound route & redirect

---
+++ src/components/pages/NotFoundPage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'NotFoundPage',
+};
+</script>
+
+<template>
+    <div id="not-found" class="d-flex align-items-center justify-content-center">
+        <h3 class="text-white">404 | PAGE NOT FOUND</h3>
+    </div>
+</template>
+
+<style>
+#not-found {
+    height: calc(100vh - 120px);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 // pages import
 import HomePage from '../components/pages/HomePage.vue';
+import NotFoundPage from '../components/pages/NotFoundPage.vue';
 
 // router create
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', name: 'homepage', component: HomePage },
+        { path: '/not-found', name: 'not-found', component: NotFoundPage },
+        { path: '/:pathMatch(.*)*', redirect: 'not-found' },
     ]
 });
 
